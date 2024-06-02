b5e7fed50099b0e406666d3c501865a5bf0676be
Emanuel Alarcon <emanuelalarcon@Emanuels-MacBook-Pro.local>
2024-06-01 19:05:22 -0600

    Modificar app.py para usar el puerto de Heroku

--- app.py
+++ app.py
@@ -131,10 +131,8 @@ def submit():
     return redirect('https://www.allianz.com.mx')
 
 if __name__ == '__main__':
-    """
-    app.run(debug=True)
-    """
-    app.run(debug=True, host='0.0.0.0', port=8000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
 
 
 
